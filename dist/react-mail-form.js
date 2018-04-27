2b75f787008903e95df6cbc04f4af870ce276bce
Evangelos Tripolitakis <e.tripolitakis@relux.com>
2018-04-27 19:38:30 +0300

    [CHG] changes to be distributable

--- dist/react-mail-form.js
+++ dist/react-mail-form.js
@@ -1937,33 +1937,22 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 
-var _react2 = _interopRequireDefault(_react);
+var _react2 = babelHelpers.interopRequireDefault(_react);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* jshint esversion: 6 */
+console.log("hello world"); /* jshint esversion: 6 */
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "React" }] */
 
-console.log("hello world");
-
 var ReactMailForm = function (_React$Component) {
-    _inherits(ReactMailForm, _React$Component);
+    babelHelpers.inherits(ReactMailForm, _React$Component);
 
     function ReactMailForm(props) {
-        _classCallCheck(this, ReactMailForm);
-
-        return _possibleConstructorReturn(this, (ReactMailForm.__proto__ || Object.getPrototypeOf(ReactMailForm)).call(this, props));
+        babelHelpers.classCallCheck(this, ReactMailForm);
+        return babelHelpers.possibleConstructorReturn(this, (ReactMailForm.__proto__ || Object.getPrototypeOf(ReactMailForm)).call(this, props));
     }
 
-    _createClass(ReactMailForm, [{
+    babelHelpers.createClass(ReactMailForm, [{
         key: "render",
         value: function render() {
             return _react2.default.createElement(
@@ -1974,7 +1963,6 @@ var ReactMailForm = function (_React$Component) {
             );
         }
     }]);
-
     return ReactMailForm;
 }(_react2.default.Component);
 
