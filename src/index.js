9d7d7ee46d04d3cc1869f514ea52dfce96a87143
Evangelos Tripolitakis <e.tripolitakis@relux.com>
2018-04-27 11:44:48 +0300

    [CHG] exporting a simple hello world

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "React" }] */
 
 import React from "react"
-import { render } from "react-dom"
+
+export default class ReactMailForm extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <div>ALOHA {this.props.formName}</div>
+    }
+}
