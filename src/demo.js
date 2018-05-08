18acef9a50501ed26a983e4a31a9823ac2a2639f
Vaggelis <vtripolitakis@gmail.com>
2018-05-08 18:01:13 +0300

    [CHG] minor fix

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,14 @@
 /* jshint esversion: 6 */
+/* global formConfigurationURL */
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "React" }] */
 
 import React from "react"
 import {render} from "react-dom"
 import ReactMailForm from "./ReactMailForm"
 
-render(<ReactMailForm formConfigurationURL="example/config.json" />, document.getElementById("root"))
+if (typeof formConfigurationURL!=="undefined"){
+    render(<ReactMailForm formConfigurationURL={formConfigurationURL} />, document.getElementById("root"))
+}else{
+    render(<ReactMailForm formConfigurationURL="example/config.json" />, document.getElementById("root"))
+}
+
