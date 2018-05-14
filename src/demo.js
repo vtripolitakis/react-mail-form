8773d1a26cc6094f83ef26553634410a9a648e69
Evangelos Tripolitakis <e.tripolitakis@relux.com>
2018-05-14 22:28:53 +0300

    [CHG] added reportValidity polyfill

--- src/demo.js
+++ src/demo.js
@@ -6,6 +6,21 @@ import React from "react"
 import {render} from "react-dom"
 import ReactMailForm from "./ReactMailForm"
 
+
+// Polyfill for reportValidity for IE
+/* Copyright (c) 2016 Tobias Buschor https://goo.gl/gl0mbf | MIT License https://goo.gl/HgajeK */
+
+if (!HTMLFormElement.prototype.reportValidity) {
+    HTMLFormElement.prototype.reportValidity = function() {
+        if (this.checkValidity()) return true
+        var btn = document.createElement("button")
+        this.appendChild(btn)
+        btn.click()
+        this.removeChild(btn)
+        return false
+    }
+}
+
 if (typeof formConfigurationURL!=="undefined"){
     render(<ReactMailForm formConfigurationURL={formConfigurationURL} />, document.getElementById("root"))
 }else{
